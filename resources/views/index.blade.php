ef7c34cfe25337d1492cd865711449998165b96d
Favour <toluwaniokunowo@gmail.com>
2022-05-03 18:20:19 +0100

    style form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,31 +6,42 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Scrapper</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
-        <div>
+
+    <body class="antialiased">
+        <div class="container mx-auto pt-12 max-w-2xl">
+            <h1 class="text-3xl font-bold text-center mb-8">Puppeteer Web Scrapper</h1>
             <form action="{{ route('store') }}" method="POST">
                 @csrf
 
-                <div>
-                    @foreach ($errors->all() as $errors)
-                        <li>
-                            {{ $errors }}
-                        </li>
-                    @endforeach
+                <div class="space-y-4">
+                    <div>
+                        @foreach ($errors->all() as $errors)
+                            <li class="text-sm text-red-600">
+                                {{ $errors }}
+                            </li>
+                        @endforeach
 
-                </div>
-                <div>
-                    <label for="selector">Selector</label>
-                    <input type="text" id="selector" name="selector" value="{{ old('selector') }}">
-                </div>
+                    </div>
 
-                <div>
-                    <label for="url">URL</label>
-                    <input type="url" id="url" name="url" value="{{ old('url') }}">
-                </div>
+                    <div class="space-y-3">
+                        <label for="selector">Selector</label>
+                        <input type="text" id="selector" class="w-2/3 flex border rounded-md py-1 px-2" name="selector"
+                               value="{{ old('selector') }}">
+                    </div>
 
-                <button type="submit">Search</button>
+                    <div class="space-y-3">
+                        <label for="url">URL</label>
+                        <input type="url" id="url" name="url" class="w-2/3 flex border rounded-md py-1 px-2"
+                               value="{{ old('url') }}">
+                    </div>
+
+                    <button type="submit"
+                            class="bg-sky-400 px-3 py-1 flex items-center rounded-lg hover:bg-sky-600 font-bold text-white">
+                        Search
+                    </button>
+                </div>
             </form>
         </div>
     </body>
