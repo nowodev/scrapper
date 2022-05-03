21aa89ef89dfad883a9e5eb52d5ab7336b4ccf01
Favour <toluwaniokunowo@gmail.com>
2022-05-03 20:18:36 +0100

    add script

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Scrapper</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}"></script>
     </head>
 
     <body class="antialiased">
